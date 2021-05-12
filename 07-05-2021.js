1e992dc11513875d968c057f5712767b8f52820e
Cemal Okten <cemalokten@gmail.com>
2021-05-12 14:10:35 +0100

    FAC Challenge 9

--- 07-05-2021.js
+++ 07-05-2021.js
@@ -7,11 +7,11 @@
 // formatNumber should return the number formatted as a readable string, inserting a comma (",") as a thousands separator. If the number n is an integer, add .0 to the formatted string, otherwise, print the decimals as they are.
 
 // Example:
-// formatNumber(1); //Expected "1.0"
+// formatNumber(1);//Expected "1.0"
 
-// formatNumber(1000.23); //Expected "1,000.23"
+// formatNumber(1000.23);//Expected "1,000.23"
 
-// formatNumber(23082342.3285); //Expected 23,082,342.3285
+// formatNumber(23082342.3285);//Expected 23,082,342.3285
 
 const formatNumber = function (number) {
 	return number.toLocaleString('en-GB', {
