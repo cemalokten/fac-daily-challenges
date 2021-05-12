e035378d8f69daf4fd992d376cc7695244fed0ea
Cemal Okten <cemalokten@gmail.com>
2021-05-12 14:08:34 +0100

    FAC Challenge 12

---
+++ 12-05-2021.js
@@ -0,0 +1,33 @@
+// Challenge: 12 (Wednesday, May 12):
+
+// Write a
+// function shoppingList that accepts a single parameter, a string.
+// The string is a list of ingredients, separated by a comma, formatted as below:
+
+//     "2 tomatoes, 1 egg, 3 pumpkins"
+
+// The
+// function shoppingList should
+// return an object where the keys are the ingredient names, and the values are the number of the ingredients needed.
+
+// Example:
+//     shoppingList("2 tomatoes, 1 egg, 3 pumpkins");
+// // returns { tomatoes: 2, egg: 1, pumpkins: 3 }
+
+// shoppingList("");
+// // returns {}
+
+// shoppingList("2 tomatoes, 1 egg, 0 pumpkins");
+// // returns { tomatoes: 2, egg: 1 }
+
+function shoppingList(string) {
+    const arr = string.split(',')
+    const obj = {}
+    arr.forEach(item => {
+        let arrItem = item.trim().split(' ')
+        obj[arrItem[0]] = arrItem[1]
+    })
+    return obj
+}
+
+console.log(shoppingList("2 tomatoes, 1 egg, 3 pumpkins"));
