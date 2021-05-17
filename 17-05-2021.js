d5ac76c5f9e6f660ce7967fcc89a87f9e4451b5c
Cemal Okten <60609268+cemalokten@users.noreply.github.com>
2021-05-17 20:05:44 +0100

    Update 17-05-2021.js

--- 17-05-2021.js
+++ 17-05-2021.js
@@ -2,7 +2,8 @@
 
 // Write a function printInBox which takes an array of strings.
 // printInBox returns undefined.
-// printInBox should log each element of the string to the console, with all the words surrounded by a box.The box should leave at least 2 spaces between the edge of the box and the longest word.The words should all be left - aligned.
+// printInBox should log each element of the string to the console, with all the words surrounded by a box.
+// The box should leave at least 2 spaces between the edge of the box and the longest word.The words should all be left - aligned.
 
 // Example:
 //     printInBox(['hello', 'there', 'my', 'friend']);
