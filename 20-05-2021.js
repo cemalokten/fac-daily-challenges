746ba50a51ede943525b88c9e500c72e79179334
Cemal Okten <cemalokten@gmail.com>
2021-05-24 10:24:33 +0100

    FAC Challenge 19

---
+++ 20-05-2021.js
@@ -0,0 +1,19 @@
+// Challenge 18 (Thursday, May 20): 
+
+// Write a function squareDigits that accepts one parameters n, a number.
+
+// Return the number that results from concatenating the square of every digit of n
+
+// Example:
+// squareDigits(10)// returns 10
+
+// squareDigits(22) // returns 44
+
+// squareDigits(95) // returns 8125
+
+function squareDigits(n) {
+    const arr = n.toString().split('');
+    return arr.map(a => Math.pow(Number(a), 2)).join('');
+}
+
+console.log(squareDigits(22));
